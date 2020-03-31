dfc31cb5b0a0851646da95d28b85be1d806c8d7b
Kanata Shimomoto <thunderpooh3@gmail.com>
2020-03-31 15:13:44 +0900

    add game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, Keyboard, Ale
 
 import Card from '../components/Card'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 import Colors from '../constants/colors'
 
 const StartGameScreen = props => {
@@ -32,12 +33,19 @@ const StartGameScreen = props => {
         setConfirmed(true);
         setSelectedNumber(chosenNumber);
         setEnteredValue('');
+        Keyboard.dismiss();
     };
 
     let confirmedOutput;
 
     if(confirmed) {
-        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+        confirmedOutput = (
+          <Card style={styles.summaryContainer}>
+            <Text>You selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="START GAME" />
+          </Card>
+      );
     }
 
     return (
@@ -107,6 +115,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: "center"
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: "center"
     }
 });
 
